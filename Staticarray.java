84096bddbc3bc2e9f5d4c5880689c91f081d2467
HasiniE2006 <hasini1530@gmail.com>
2024-12-07 13:28:57 +0530

    Add files via upload

---
+++ Staticarray.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Staticarray 
+{
+    public static void main(String args[])
+    {
+        //intilization
+        int arr[]={11,17,18,19,2};       
+        System.out.println(Arrays.toString(arr));
+        //modification
+        arr[2]=102;
+        System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
